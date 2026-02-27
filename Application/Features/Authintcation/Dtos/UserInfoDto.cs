09abef488496de2bae7ab9e86c637388871d1869
noouh1 <ae2303@fayoum.edu.eg>
2026-02-27 04:50:18 +0200

    [Fix]:add userinfo in login

---
+++ Application/Features/Authintcation/Dtos/UserInfoDto.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enums;
+
+namespace Application.Features.Identities.Dtos;
+
+public record UserInfoDto(
+    Ulid Id,
+    string Name,
+    string Email,
+    bool EmailConfirmed,
+    string PhoneNumber,
+    string? ProfileImage,
+    bool LockoutEnabled,
+    DateTimeOffset? LockoutEnd,
+    DateTime? LastLoginAt,
+    string? NID,
+    int Age,
+    Gender? Gender,
+    IList<string> Roles
+);
+
