8f3d6ee5f0e90c68cb7e4733f8f667667d4ffe00
mkhaled1212 <174843539+mkhaled1212@users.noreply.github.com>
2026-03-05 02:03:50 -0800

    addsalary

---
+++ Web.Api/Controllers/SalaryController/SalaryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Api.Controllers.SalaryController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalaryController : ControllerBase
+    {
+    }
+}
